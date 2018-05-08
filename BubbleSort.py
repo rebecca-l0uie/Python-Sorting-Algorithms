754baa8b5d94e1173257a1ebfe1863af9af3e5d9
Rebecca Louie <39102589+rebecca-l0uie@users.noreply.github.com>
2018-05-08 13:37:02 -0700

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,18 @@
+#usr/bin/python
+##########################
+# Author: Rebecca Louie
+# Date  : May 13 2016
+##########################
+import random
+
+def bubsort(l):
+	swaooing=true
+	while swaping:
+		SWAPING=FLASE
+		FOR X IN range(len(l)-1):
+			if l[x]>l[x+1]
+				l[x],l[x+1]=l[x+1],l[x]
+				swapping = True 
+
+a=random.sample(range(100), 10)
+bubsort(a)
